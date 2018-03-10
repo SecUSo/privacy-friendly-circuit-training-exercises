ca960f8502761b1f3d8306a913301a999cd5e7d3
Nils Schroth <nils.schroth@googlemail.com>
2018-03-10 20:49:40 +0100

    changed app name in license

--- app/src/main/java/org/secuso/privacyfriendlycircuittraining/fragments/MonthlyReportFragment.java
+++ app/src/main/java/org/secuso/privacyfriendlycircuittraining/fragments/MonthlyReportFragment.java
@@ -1,10 +1,10 @@
 /**
- * This file is part of Privacy Friendly Interval Timer.
- * Privacy Friendly Interval Timer is free software:
+ * This file is part of Privacy Friendly Circuit Trainer.
+ * Privacy Friendly Circuit Trainer is free software:
  * you can redistribute it and/or modify it under the terms of the
  * GNU General Public License as published by the Free Software Foundation,
  * either version 3 of the License, or any later version.
- * Privacy Friendly Interval Timer is distributed in the hope
+ * Privacy Friendly Circuit Trainer is distributed in the hope
  * that it will be useful, but WITHOUT ANY WARRANTY; without even
  * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
