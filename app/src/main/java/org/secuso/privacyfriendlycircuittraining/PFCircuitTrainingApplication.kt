8d562dd0106972276184173f4ab8726b73969e42
Tobias Länge <tobias.laenge9@kit.edu>
2022-10-27 14:14:07 +0200

    Implement backup integration
    - Add SCHEDULE_EXACT_ALARM permission
    - Add FLAG_IMMUTABLE to PendingIntents

---
+++ app/src/main/java/org/secuso/privacyfriendlycircuittraining/PFCircuitTrainingApplication.kt
@@ -0,0 +1,21 @@
+package org.secuso.privacyfriendlycircuittraining
+
+import android.app.Application
+import android.util.Log
+import androidx.work.Configuration
+import org.secuso.privacyfriendlybackup.api.pfa.BackupManager
+import org.secuso.privacyfriendlycircuittraining.backup.BackupCreator
+import org.secuso.privacyfriendlycircuittraining.backup.BackupRestorer
+
+class PFCircuitTrainingApplication : Application(), Configuration.Provider {
+
+    override fun onCreate() {
+        super.onCreate()
+        BackupManager.backupCreator = BackupCreator()
+        BackupManager.backupRestorer = BackupRestorer()
+    }
+
+    override fun getWorkManagerConfiguration(): Configuration {
+        return Configuration.Builder().setMinimumLoggingLevel(Log.INFO).build()
+    }
+}
