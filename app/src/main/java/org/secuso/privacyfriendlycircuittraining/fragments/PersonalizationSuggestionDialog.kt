bd25ce083137a42b0b30867d94b581bf89ef5968
betul cuhadar <bcuhadar7@gmail.com>
2026-07-06 11:40:39 +0200

    add suggestion dialog

---
+++ app/src/main/java/org/secuso/privacyfriendlycircuittraining/fragments/PersonalizationSuggestionDialog.kt
@@ -0,0 +1,18 @@
+package org.secuso.privacyfriendlycircuittraining.fragments
+
+import androidx.appcompat.app.AppCompatActivity
+import org.secuso.pfacore.model.dialog.AbortElseDialog
+import org.secuso.pfacore.ui.dialog.show
+import org.secuso.privacyfriendlycircuittraining.R
+
+object PersonalizationSuggestionDialog {
+    @JvmStatic
+    fun show(activity: AppCompatActivity) {
+        AbortElseDialog.build(activity) {
+            title       = { activity.getString(R.string.alert_personalization_title) }
+            content     = { activity.getString(R.string.alert_personalization_message) }
+            acceptLabel = activity.getString(R.string.alert_confirm_dialog_positive)
+            abortLabel  = activity.getString(R.string.alert_confirm_dialog_negative)
+        }.show()
+    }
+}
